0793cb6d37a0d669e51737cd0a4e6a5ada35ddd0
Kireev, Dmitriy <dmitriy.kireev@atos.net>
2019-02-10 14:08:44 +0300

    Initial commit

---
+++ TestEventReceiver/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="c8362e75-9b3a-482b-896b-51d645a8a8fe" featureId="c8362e75-9b3a-482b-896b-51d645a8a8fe" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="TestEventReceiver Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="6d1f9c40-ef2c-47bf-ac56-99ceb5cf959b" />
+  </projectItems>
+</feature>
---
+++ TestEventReceiver/MyEventReceiver/MyEventReceiver.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace TestEventReceiver.MyEventReceiver
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class MyEventReceiver : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item is being added.
+        /// </summary>
+        public override void ItemAdding(SPItemEventProperties properties)
+        {
+            //base.ItemAdding(properties);  
+            using (SPWeb web = properties.OpenWeb())
+            {
+                try
+                {
+                    SPList list = web.Lists["EmployeeDocumentLog"];
+                    SPListItem newItem = list.Items.Add();
+                    newItem["Title"] = properties.ListItem.Name;
+                    newItem["DocumentAction"] = "Document Added";
+                    newItem.Update();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        /// <summary>
+        /// An item is being updated.
+        /// </summary>
+        public override void ItemUpdating(SPItemEventProperties properties)
+        {
+            //base.ItemUpdating(properties);
+            using (SPWeb web = properties.OpenWeb())
+            {
+                try
+                {
+                    SPList list = web.Lists["EmployeeDocumentLog"];
+                    SPListItem newItem = list.Items.Add();
+                    newItem["Title"] = properties.ListItem.Name;
+                    newItem["DocumentAction"] = "Document Updated";
+                    newItem.Update();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        /// <summary>
+        /// An item is being deleted.
+        /// </summary>
+        public override void ItemDeleting(SPItemEventProperties properties)
+        {
+            //base.ItemDeleting(properties);  
+            using (SPWeb web = properties.OpenWeb())
+            {
+                try
+                {
+                    SPList list = web.Lists["EmployeeDocumentLog"];
+                    SPListItem newItem = list.Items.Add();
+                    newItem["Title"] = properties.ListItem.Name;
+                    newItem["DocumentAction"] = "Document Deleted";
+                    newItem.Update();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+
+    }
+}
